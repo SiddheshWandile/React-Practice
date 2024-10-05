09e9da24bbbfb9c18adf77682110870d2ea65f98
Siddhesh Milind Wandile <siddheshwandile@gmail.com>
2024-10-05 19:26:38 +0530

    ReduxToolkit

---
+++ ReduxToolkit/src/App.jsx
@@ -0,0 +1,14 @@
+import AddTodos from "./components/AddTodos";
+import Todos from "./components/Todos";
+
+function App() {
+  return (
+    <>
+      <h1>Learn about toolkit</h1>
+      <AddTodos />
+      <Todos />
+    </>
+  );
+}
+
+export default App;
